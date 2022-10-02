f62a4ab4076148ec0eb38f795b1158342d62cd05
Daniel Liu <75332824+DanL2015@users.noreply.github.com>
2022-10-02 12:43:43 -0700

    Update window.sh

--- polybar/scripts/window.sh
+++ polybar/scripts/window.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WM_DESKTOP=$(xdotool getwindowfocus)
 
-if [ $WM_DESKTOP == "4194404" ]; then
+if [ $WM_DESKTOP == "Desktop Value" ]; then
 	echo "Desktop"
 else
 	echo $(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
