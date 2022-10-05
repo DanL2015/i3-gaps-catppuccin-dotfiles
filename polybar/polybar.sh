ad9357cd1c7b606b6f062b7a69a489d5622b5ea6
Daniel Liu <daniel17liu@gmail.com>
2022-10-05 14:12:30 -0700

    changed rounded corners

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -7,6 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 chmod +x $HOME/.config/polybar/scripts/window.sh
+chmod +x $HOME/.config/polybar/scripts/get_spotify_status.sh
+chmod +x $HOME/.config/polybar/scripts/scroll_spotify_status.sh
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar &
--- polybar/scripts/window.sh
+++ polybar/scripts/window.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 WM_DESKTOP=$(xdotool getwindowfocus)
 
-if [ $WM_DESKTOP == "Desktop Value" ]; then
+if [ $WM_DESKTOP == "4194404" ]; then
 	echo "Desktop"
 else
-	echo $(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
+	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
+	echo $WM_NAME
 fi
